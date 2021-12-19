66c5b98151afa2e17fe3aa6e0636986c54b58bd3
Angus Robinson <angus@fairhaven.za.net>
2021-12-19 18:39:01 +0200

    moving over from azure repo

---
+++ functions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+__author__ = "Angus Robinson"
+__copyright__ = "Copyright 2019, Sargus Networks"
+__credits__ = ["Angus Robinson", "All those that provided answers and docuumentation",
+                "on the internet"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Angus Robinson"
+__email__ = "angus@sargus.net.za"
+__status__ = "Production"
